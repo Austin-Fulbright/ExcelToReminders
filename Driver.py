8e08e7dc3d6ccb7a17ef9a1c50045df5e86d83cf
Austin Fulbright <afulbright@network21.local>
2020-11-17 12:26:16 -0500

    added Driver

--- Driver.py
+++ Driver.py
@@ -1,2 +1,18 @@
-from excel_to_reminders import *
+from reminder_Create import *
+import pandas as pd
+import datetime
 
+#9/1/2020
+recp = input("Enter Recipient(email) of Reminders: ")
+xlsF = input("Enter excel document name: ")
+#yyyy-MM-dd hh:mm
+aptdf = pd.read_excel(xlsF)
+#Creates reminders for two years
+for index, row in aptdf.iterrows():
+    location = row["PhysicalName"]
+    subject = row["Reminder"]
+    dates = format_date(row["Completed"])
+    inter = row["FreqMonths"]
+    occur = calcOccur(inter)
+    sendRecurringMeeting(recp, subject, dates, location, inter, occur)
+#newdate = format_date()
