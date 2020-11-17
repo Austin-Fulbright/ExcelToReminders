1188d22df3fed360092e61d44ff63d4c97833d06
Austin Fulbright <afulbright@network21.local>
2020-11-17 10:26:21 -0500

    created outlook recuring meeting section in excel_to_reminders.py

--- .ipynb_checkpoints/excel_to_reminders-checkpoint.py
+++ .ipynb_checkpoints/excel_to_reminders-checkpoint.py
@@ -2,14 +2,31 @@ import win32com.client
 import pandas as pd
 outlook = win32com.client.Dispatch("Outlook.Application")
 
-def sendEmail():
+'''def sendEmail():
     Msg = outlook.CreateItem(0) # Email
     Msg.To = "jfulbright@network21.com" # you can add multiple emails with the ; as delimiter. E.g. test@test.com; test2@test.com;
     Msg.CC = "ajf73130@uga.edu"
     Msg.Subject = "Test"
     Msg.Body = "This is a Test"
     Msg.Send()
+'''
 
+'''def setupAppointment(date, title, recipients, prop, freq):    
+    appt = outlook.CreateItem(1) # AppointmentItem
+    appt.Start = date #"2020-11-28 10:10 yyyy-MM-dd hh:mm
+    appt.Subject = title
+    appt.Duration = 1 # In minutes (60 Minutes)
+    appt.Location = prop
+    appt.Recipients.Add(recipients) # Don't end ; as delimiter
+
+  # Set Pattern, to recur every day, for the next 5 days
+    pattern = appt.GetRecurrencePattern()
+    pattern.RecurrenceType = 0
+    pattern.Interval = freq
+    pattern.Occurrences = "5"
+    appt.Save()
+    appt.Send()
+'''
 def sendRecurringMeeting(rec, title, date, prop, freq):    
     appt = outlook.CreateItem(1) # AppointmentItem
     appt.Start = date # yyyy-MM-dd hh:mm
@@ -29,8 +46,5 @@ def sendRecurringMeeting(rec, title, date, prop, freq):
 
     appt.Save()
     appt.Send()
-
 #setupAppointment("2020-11-28 10:10", "apt", "ajf73130@uga.edu", "my prop", 2)
-#sendRecurringMeeting("ajf73130@uga.edu","meeting","2020-11-28 10:10","my house",2)
-
-
+sendRecurringMeeting("ajf73130@uga.edu","meeting","2020-11-28 10:10","my house",2)
